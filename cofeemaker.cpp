48fcd668b4c55091cef3d324579325063edc40a3
Riya Dennis <riyacv@gmail.com>
2019-03-11 16:27:03 +0000

    added factory pattern to create objects

--- cofeemaker.cpp
+++ cofeemaker.cpp
@@ -11,45 +11,10 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    Coffee c;
-    int cType;
-    bool success;
-    cout << "Please select your drink" << endl;
-    cout << "1. Cappuccino" << endl;
-    cout << "2. Esspresso" << endl;
-    cout << "3. Americano" << endl;
-    cout << "4. Latte" << endl;
-    cin >> cType;
-    
-    Milk m;
-    m.setMilkType("Soya");
-    
-    switch (cType) {
-        case 1: {
-            Cappuccino ca;
-            c.setCoffeeType("Cappuccino");
-            ca.SetMilk(m);
-            ca.Make(m);
-            success = true;
-            break;
-        }
-        case 2: {
-            Esspresso e;
-            e.Make();
-            c.setCoffeeType("Esspresso");
-            success = true;
-            break;
-        }
-        default:{
-            cout << "machine not ready to process your option" << endl;
-            success = false;
-            break;
-        }
-    }
-    status(20);
-    if (success){
-        cout << "Your " << c.getCofeeType() << " is ready "<< endl;
-    }
-    
+    CofeeMaker cm;
+    Coffee *c;
+    c = cm.getCoffee();
+    cout << "Your " << c->getCofeeType() << " is ready "<< endl;
     return 0;
 }
+
--- coffemaker.h
+++ coffemaker.h
@@ -22,11 +22,21 @@ public:
 };
 
 class Coffee {
-private:
+protected:
     string dType;
 public:
+    Coffee(){};
+    string getType(){return dType;}
     void setCoffeeType(string dt){dType = dt;}
     string getCofeeType(){return dType;};
+    void status(int timer){
+        for (int i=0; i< timer; i++){
+            cout << ".";
+            fflush(stdout);
+            sleep(1);
+        }
+        cout << endl;
+    }
     
 };
 
@@ -35,12 +45,15 @@ private:
     string flavour;
     Milk m;
 public:
-    void SetMilk(Milk cm){m=cm;}
-    void Make(Milk m){
-        if (m.getMilkType() == "") {
-            m.setMilkType("normal");
-        }
-        cout << "making " << "Cappuccino" << " using milk " << m.getMilkType() <<endl;
+    Cappuccino() : Coffee(){
+        this->dType = "Cappuccino";
+        cout << endl << "Making a cup of cappuccino" << endl;
+        cout << "Grind and brew one scoop of espresso beans." << endl;
+        cout << "Heat and foam milk." << endl;
+    }
+    void SetMilk(Milk cm){
+        m=cm;
+        cout << " using milk " << m.getMilkType() <<endl;
     }
 };
 
@@ -48,17 +61,40 @@ class Esspresso: public Coffee{
 private:
     string flavour;
 public:
-    void Make(){
-        cout << "making " << "Esspresso" <<endl;
+    Esspresso(): Coffee(){
+        this->dType = "Esspresso";
+        cout << endl << "Making a cup of espresso" << endl;
+        cout << "Grind and brew one scoop of espresso beans." << endl;
     }
 };
 
-void status(int timer){
-    for (int i=0; i< timer; i++){
-        cout << ".";
-        fflush(stdout);
-        sleep(1);
+
+class CofeeMaker{
+private:
+    Coffee * coffee;
+public:
+    Coffee * getCoffee(){
+        int cType;
+        cout << "Please select your drink" << endl;
+        cout << "1. Cappuccino" << endl;
+        cout << "2. Esspresso" << endl;
+        cout << "3. Americano" << endl;
+        cout << "4. Latte" << endl;
+        cin >> cType;
+        switch (cType) {
+                case 1:
+                    return new Cappuccino();
+                case 2:
+                return new Esspresso();
+                default:{
+                    cout << "machine not ready to process your option" << endl;
+                    return NULL;
+                }
+        }
+        return NULL;
     }
-    cout << endl;
-}
+};
+
+
+
 #endif /* coffemaker_h */
